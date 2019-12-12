a5b1f58c3d37900dd76a881f72df0cc5bfc9c74b
Mitch Stares <mitchell.stares@hotmail.com>
2019-12-12 16:57:35 +1100

    Sketch independant! :tada: Moved sketch build files to new folder

--- R/quizlite.R
+++ R/quizlite.R
@@ -8,10 +8,10 @@
 #'
 quizlite <- function(quiz_db){
   dir0 <- tempdir()
-  x <- sketch::source_r("inst/main.R")
-  html <- "index.html"
+  #x <- sketch::source_r("inst/main.R")
+  html <- system.file("index.html", package = "quizlite")
   jsonlite::write_json(quiz_db, paste0(dir0, "/quiz_db.json"), auto_unbox = T)
-  file.copy(html, file.path(dir0, html))
-  #browseURL(file.path(dir0, html))
-  htmltools::doRenderTags(htmltools::tags$iframe(src = file.path(dir0, html), style = "border: none;"))
+  file.copy(html, file.path(dir0, "index.html"))
+  browseURL(file.path(dir0, "index.html"))
+  #htmltools::doRenderTags(htmltools::tags$iframe(src = file.path(dir0, html), style = "border: none;"))
 }
