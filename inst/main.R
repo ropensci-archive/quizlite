715e69e757342c5a693f1a987a3bb39e73fefd1b
Mitch Stares <mitchell.stares@hotmail.com>
2019-12-12 15:46:56 +1100

    Add example.rmd, fix filepaths inside main.R

--- inst/main.R
+++ inst/main.R
@@ -1,8 +1,8 @@
 #! load_script("https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js")
 # Source: https://github.com/jchamill/jquery-quiz
-#! load_script("./jquery.quiz.css")
-#! load_script("./jquery.quiz.js")
-#! load_script("simple_interface.R")
+#! load_script("inst/jquery.quiz.css")
+#! load_script("inst/jquery.quiz.js")
+#! load_script("inst/simple_interface.R")
 jQuery$getJSON(
   "quiz_db.json",
   function(quiz_db_json) {
