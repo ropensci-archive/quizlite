3dd582cad8c62f293f04f05fca482232e2b9bcf6
Mitch Stares <mitchell.stares@hotmail.com>
2019-12-12 16:33:42 +1100

    tidying up the example.rmd and tidy up the quizlite function

--- R/quizlite.R
+++ R/quizlite.R
@@ -10,10 +10,8 @@ quizlite <- function(quiz_db){
   dir0 <- tempdir()
   x <- sketch::source_r("inst/main.R")
   html <- "index.html"
-
-  # convert `quiz_db` into a JSON and write to dir0 here
-
   jsonlite::write_json(quiz_db, paste0(dir0, "/quiz_db.json"), auto_unbox = T)
   file.copy(html, file.path(dir0, html))
-  browseURL(file.path(dir0, html))
+  #browseURL(file.path(dir0, html))
+  htmltools::doRenderTags(htmltools::tags$iframe(src = file.path(dir0, html), style = "border: none;"))
 }
