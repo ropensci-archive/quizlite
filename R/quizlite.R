0c34310c5c117f2e00646e07f4e3056d097da11a
Mitch Stares <mitchell.stares@hotmail.com>
2019-12-13 08:51:51 +1100

    Update css

--- R/quizlite.R
+++ R/quizlite.R
@@ -8,7 +8,7 @@
 #'
 quizlite <- function(quiz_db, ... ){
   dir0 <- tempdir()
-  #x <- sketch::source_r("inst/unused/main.R")
+  x <- sketch::source_r("inst/unused/main.R")
   html <- system.file("index.html", package = "quizlite")
   jsonlite::write_json(quiz_db, file.path(dir0, "quiz_db.json"), auto_unbox = T)
   file.copy(html, file.path(dir0, "index.html"))
