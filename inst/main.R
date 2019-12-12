d934e06334bb83b5979653f5dd4bbf04ed9d258f
Mitch Stares <mitchell.stares@hotmail.com>
2019-12-12 15:42:20 +1100

    oops, fixing incorrect main.R push

--- inst/main.R
+++ inst/main.R
@@ -1,31 +1,11 @@
 #! load_script("https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js")
-
 # Source: https://github.com/jchamill/jquery-quiz
-#! load_script("inst/jquery.quiz.css")
-#! load_script("inst/jquery.quiz.js")
-
-#! load_script("inst/simple_interface.R")
-quiz_db <- list(
-  counterFormat = 'Question %current of %total',
-  questions = c(
-    list(
-      q= 'A sample question?',
-      options= c('Answer 1',
-                 'Answer 2',
-                 'Answer 3',
-                 'Answer 4'),
-      correctIndex = 1,
-      correctResponse = 'Custom correct response.',
-      incorrectResponse = 'Custom incorrect response.'
-    ),
-    list(
-      q= 'A new question?',
-      options= c('Answer 1', 'Answer 2'),
-      correctIndex = 0,
-      correctResponse = 'Custom correct response.',
-      incorrectResponse = 'Custom incorrect response.'
-    )
-  )
+#! load_script("./jquery.quiz.css")
+#! load_script("./jquery.quiz.js")
+#! load_script("simple_interface.R")
+jQuery$getJSON(
+  "quiz_db.json",
+  function(quiz_db_json) {
+    jQuery('#new_sketch')$quiz(quiz_db_json)
+  }
 )
-
-jQuery('#new_sketch')$quiz(quiz_db)
