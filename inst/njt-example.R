85a5b9c5c947bfa733023a2cf4ba4d1aee435cc2
Nicholas Tierney <nicholas.tierney@gmail.com>
2019-12-13 11:43:48 +1100

    list magics

--- inst/njt-example.R
+++ inst/njt-example.R
@@ -1,10 +1,16 @@
-quizlite_test <- function(...){
+quizlite_test <- function(...,
+                          correct_response = 'Custom correct response.',
+                          incorrect_response = 'Custom incorrect response.'
+                          ){
+  dots <- rlang::dots_list(...)[[1]]
+
   list(
     counterFormat = 'Question %current of %total', #Do not modify
-    questions = list(...# this is where our question list structure is generated
-        ),
-    options = c(...), # Takes the input from questions list, grabbing the named options vector
-    correctIndex = match("TRUE", options)
+    questions = dots[["q"]], # this is where our question list structure is generated
+    options = dots[["options"]], # Takes the input from questions list, grabbing the named options vector
+    correctIndex = (match("TRUE", dots[["options"]]) - 2),
+    correctResponse = correct_response,
+    incorrectResponse = incorrect_response
   )
 }
 
@@ -26,8 +32,13 @@ answer <- function(text,
 
 }
 
-debugonce(question)
-list_quiz <- quizlite_test(question(text = "What is better?", answer("R", correct = TRUE), answer("Python")))
+debugonce(quizlite_test)
+list_quiz <-
+  quizlite_test(
+    question(text = "What is better?",
+             answer("R", correct = TRUE),
+             answer("Python"))
+    )
 
 
 
